4c37b85e912c81a4d7f6c9acc1b5af2d3e6aadda
Bagrat <bagratavanesian@yandex.ru>
2023-05-04 16:27:06 +0300

    fix: codeStyle

--- stats/stats-dto/src/main/java/ru/practicum/validation/StatisticValidation.java
+++ stats/stats-dto/src/main/java/ru/practicum/validation/StatisticValidation.java
@@ -16,7 +16,7 @@ public class StatisticValidation {
     private Pattern datePattern = Pattern.compile("^(20[23][0-9])-([0][1-9]|[1][012])-([12][0-9]|[3][01]|[0][1-9]) " +
             "([0][1-9]|[1][0-9]|[2][0-3]|[0][0])[:]([0-5][0-9])[:]([0-5][0-9])$");
 
-    public boolean ipIsValid (String ip) {
+    public boolean ipIsValid(String ip) {
         if (!ipPattern.matcher(ip).matches()) {
             throw new BadRequest("Невалидный IP адрес.");
         }
