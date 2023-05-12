675e57ddff0baba1ea87b3b2b8daf1f4a84e38b7
Bagrat <bagratavanesian@yandex.ru>
2023-05-12 23:43:05 +0300

    feat: Доработать Admin-part по Category

--- main-service/src/main/java/ru/practicum/category/dto/NewCategoryDto.java
+++ main-service/src/main/java/ru/practicum/category/dto/NewCategoryDto.java
@@ -1,11 +1,7 @@
 package ru.practicum.category.dto;
 
-import lombok.Builder;
 import lombok.Data;
-
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 @Data
 public class NewCategoryDto {
