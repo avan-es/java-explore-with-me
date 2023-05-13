83ccf4de974de61cf625fa049ec19c819dba1c2d
Bagrat <bagratavanesian@yandex.ru>
2023-05-13 12:26:45 +0300

    feat: модифицировать LocationDto

--- main-service/src/main/java/ru/practicum/event/dto/location/LocationDto.java
+++ main-service/src/main/java/ru/practicum/event/dto/location/LocationDto.java
@@ -1,7 +1,12 @@
-package ru.practicum.location;
+package ru.practicum.event.dto.location;
 
 //Широта и долгота места проведения события
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class LocationDto {
     //Широта
     private Float lat;
