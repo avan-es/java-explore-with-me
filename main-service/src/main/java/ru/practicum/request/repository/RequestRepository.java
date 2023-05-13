db441cd67f0ca0065b6eca3d3fe8b50697c4c81b
Bagrat <bagratavanesian@yandex.ru>
2023-05-13 18:59:22 +0300

    feat: работа с запросами (Private)

--- main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.practicum.request.model.Request;
 
+import java.util.List;
+
 
 @Repository(value = "dbRequestRepository")
 public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    List<Request> findAllByRequesterId (Long userId);
 
 }
