744a83022a72d0fe532fb47a797b1b9e061e7d6d
Bagrat <bagratavanesian@yandex.ru>
2023-05-18 13:05:39 +0300

    fix: обработка ошибок с Git

--- main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
@@ -1,6 +1,7 @@
 package ru.practicum.request.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.request.enums.RequestStatus;
 import ru.practicum.request.model.Request;
@@ -17,4 +18,12 @@ public interface RequestRepository extends JpaRepository<Request, Long> {
     List<Request> getByEventIdAndStatus(Long id, RequestStatus pending);
 
     List<Request> findAllByIdInAndStatus(List<Long> ids, RequestStatus status);
+
+    @Query(value =
+            "SELECT * " +
+            "FROM requests AS r " +
+            "WHERE r.user_id = ?1 " +
+            "AND r.event_id = ?2 " +
+            "LIMIT 1", nativeQuery = true)
+    Request findByUserIdAndEventId(Long userId, Long eventId);
 }
