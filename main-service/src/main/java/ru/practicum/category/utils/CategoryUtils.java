99ca1b089d570dad5d62384ee23cb4fe7bc36b1f
Bagrat <bagratavanesian@yandex.ru>
2023-05-19 17:12:40 +0300

    feat: декомпозиция Category

--- main-service/src/main/java/ru/practicum/category/utils/CategoryUtils.java
+++ main-service/src/main/java/ru/practicum/category/utils/CategoryUtils.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.practicum.ApiError.exception.BadRequestException;
+import ru.practicum.ApiError.exception.ConflictException;
 import ru.practicum.ApiError.exception.NotFoundException;
 import ru.practicum.category.dto.CategoryDto;
 import ru.practicum.category.model.Category;
 import ru.practicum.category.model.CategoryMapper;
 import ru.practicum.category.repository.CategoryRepository;
+import ru.practicum.event.repository.EventRepository;
 
 @Component
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ import ru.practicum.category.repository.CategoryRepository;
 public class CategoryUtils {
 
     private final CategoryRepository categoryRepository;
+    private final EventRepository eventRepository;
 
     public Category getCategoryModelById(Long catId) {
         log.info("Получение категории по ID = {}.", catId);
@@ -34,6 +37,20 @@ public class CategoryUtils {
         if (categoryRepository.getCategoryById(catId) == null) {
             log.error("Категория c ID = {} не существует.",catId);
             throw new BadRequestException("Категория c ID = " + catId + " не существует.");
+        }
+    }
+
+    public void isCategoryNameIsBusy(String name) {
+        if (categoryRepository.findFirstByName(name) != null) {
+            log.error("Категория \"{}\" уже существует.",name);
+            throw new ConflictException("Категория уже существует.");
+        }
+    }
+
+    public void isCategoryUsing(Long catId) {
+        if (eventRepository.findFirstByCategory(catId) != null) {
+            log.error("Категория c ID = {} используется и не может быть удалена.",catId);
+            throw new ConflictException("Категория c ID = " + catId + " используется и не может быть удалена.");
         }
     }
 }
