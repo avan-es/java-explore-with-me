bf8e3e134dfd1896ca93c510513ccbfa8fb47c4b
Bagrat <bagratavanesian@yandex.ru>
2023-05-19 17:02:32 +0300

    feat: декомпозиция Compilation

--- main-service/src/main/java/ru/practicum/compilation/service/adminPart/CompilationAdminService.java
+++ main-service/src/main/java/ru/practicum/compilation/service/adminPart/CompilationAdminService.java
@@ -1,24 +1,16 @@
-package ru.practicum.compilation.service;
+package ru.practicum.compilation.service.adminPart;
 
 
 import ru.practicum.compilation.dto.CompilationDto;
 import ru.practicum.compilation.dto.NewCompilationDto;
 import ru.practicum.compilation.dto.UpdateCompilationRequest;
-import ru.practicum.compilation.model.Compilation;
 
-import java.util.List;
-
-public interface CompilationService {
+public interface CompilationAdminService {
 
     CompilationDto createCompilation(NewCompilationDto newCompilation);
 
     CompilationDto updateCompilationById(Long compId, UpdateCompilationRequest updatedCompilation);
 
-    Compilation getCompilationById(Long compId);
-
     void deleteComplication(Long compId);
 
-    CompilationDto getCompilationByIdPublic(Long compId);
-
-    List<CompilationDto> getComplicationsPublic(Boolean pinned, Integer from, Integer size);
 }
--- main-service/src/main/java/ru/practicum/compilation/service/adminPart/CompilationAdminServiceImpl.java
+++ main-service/src/main/java/ru/practicum/compilation/service/adminPart/CompilationAdminServiceImpl.java
@@ -1,18 +1,16 @@
-package ru.practicum.compilation.service;
+package ru.practicum.compilation.service.adminPart;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.ApiError.exception.NotFoundException;
 import ru.practicum.compilation.dto.CompilationDto;
 import ru.practicum.compilation.dto.NewCompilationDto;
 import ru.practicum.compilation.dto.UpdateCompilationRequest;
 import ru.practicum.compilation.model.Compilation;
 import ru.practicum.compilation.model.CompilationMapper;
 import ru.practicum.compilation.repository.CompilationRepository;
+import ru.practicum.compilation.utils.CompilationUtils;
 import ru.practicum.event.model.Event;
 import ru.practicum.event.utils.EventUtils;
 
@@ -24,11 +22,13 @@ import java.util.Optional;
 @Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class CompilationServiceImpl implements CompilationService {
+public class CompilationAdminServiceImpl implements CompilationAdminService {
 
     private final CompilationRepository compilationRepository;
 
     private final EventUtils eventUtils;
+
+    private final CompilationUtils compilationUtils;
 
 
     @Override
@@ -45,7 +45,7 @@ public class CompilationServiceImpl implements CompilationService {
     @Transactional
     public CompilationDto updateCompilationById(Long compId, UpdateCompilationRequest updatedCompilation) {
         log.info("Администратор обновляет подборку с ID = {}.", compId);
-        Compilation compilationOld = getCompilationById(compId);
+        Compilation compilationOld = compilationUtils.getCompilationById(compId);
         List<Event> events = eventUtils.getEventByIds(updatedCompilation.getEvents());
         Optional.ofNullable(updatedCompilation.getPinned()).ifPresent(compilationOld::setPinned);
         Optional.ofNullable(updatedCompilation.getTitle()).ifPresent(compilationOld::setTitle);
@@ -57,34 +57,12 @@ public class CompilationServiceImpl implements CompilationService {
             return CompilationMapper.INSTANT.toCompilationDto(compilation);
     }
 
-    @Override
-    public Compilation getCompilationById(Long compId) {
-        log.info("Получение подборки по ID = {}.", compId);
-        return compilationRepository.findById(compId).orElseThrow(
-                () -> new NotFoundException("Подборка с ID = " + compId + " не найдена.")
-        );
-    }
 
     @Override
     @Transactional
     public void deleteComplication(Long compId) {
         log.debug("Администратор удаляет подборку с ID = {}.", compId);
         compilationRepository.deleteById(compId);
-    }
-
-    @Override
-    public CompilationDto getCompilationByIdPublic(Long compId) {
-        return CompilationMapper.INSTANT.toCompilationDto(getCompilationById(compId));
-    }
-
-    @Override
-    public List<CompilationDto> getComplicationsPublic(Boolean pinned, Integer from, Integer size) {
-        int page = from / size;
-        PageRequest pageRequest = PageRequest.of(page, size);
-        log.info("Выгрузка списка подборок с параметрами: pinned = {}, size={}, from={}.",pinned, size, page);
-        Page<Compilation> pageCompilation = compilationRepository.findAllByPinned(pinned, pageRequest);
-        List<Compilation> requests = pageCompilation.getContent();
-        return CompilationMapper.INSTANT.toCompilationDto(requests);
     }
 
 }
